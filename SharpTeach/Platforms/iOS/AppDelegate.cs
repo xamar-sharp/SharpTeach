fe3930fabe3ea49f8d3df39ee3393ffd17fe5ae7
xamar-sharp <94056606+xamar-sharp@users.noreply.github.com>
2025-04-21 11:31:22 +0300

    Add project files.

---
+++ SharpTeach/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SharpTeach
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
